db0dcbcdd7b8aaf8b48355ecd6a9964632710a50
tejaschaudhari7 <chaudharit811@gmail.com>
2022-06-29 15:12:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const pa11y = require('pa11y')
+const express = require('express');
+const app = express();
+const Port = process.env.PORT || 3000;
+
+app.use(express.static('public'))
+
+app.get('/api/test',async (req,res) => {
+    if(!req.query.url) {
+        res.status(400).json({error : 'url is required'})
+    } else {
+        const results = await pa11y(req.query.url);
+        console.log(results);
+        res.status(200).json(results);
+    }
+})
+
+app.listen(Port, () => {
+    console.log(`Up and running on ${Port}`)
+})
